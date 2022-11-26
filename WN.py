663ce630ab48a23759ebc20103f050bdf1bf4491
Dmitry <dimalimankin@gmail.com>
2022-11-26 23:52:11 +0200

    Change main_py Logic

--- WN.py
+++ WN.py
@@ -1,6 +1,6 @@
-name =input('enter NAME :')
-age =input('Enter your AGE :')
-print(F'Hello ,{name}!you are  {age}years old')
+name = input('enter NAME :')
+age = input('Enter your AGE :')
+print(F'Hello ,{name} !you are  {age}years old')
 
 
 
