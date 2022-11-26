47ff6e858e47ee544d43337ff82b6d2c1c8bdd9f
Dmitry <dimalimankin@gmail.com>
2022-11-26 22:51:18 +0200

    upd n#1

--- WN.py
+++ WN.py
@@ -1,2 +1,15 @@
-print('hello world')
-print(not False)
+# print('hello world')
+# print(not False)
+
+a = int(input('Enter number #1: '))
+b = int(input('Enter number #2: '))
+print(a + b)
+
+
+
+
+
+
+
+
+
