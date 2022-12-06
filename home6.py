c5e27eccfebcef08ed3c5a48317b99f525773674
Dmitry <dimalimankin@gmail.com>
2022-12-06 22:44:38 +0200

    add home6.py

---
+++ home6.py
@@ -0,0 +1,57 @@
+
+'''6.1'''
+# word = input('Enter your word:')
+# a = word[::-1]
+# if word == a:
+#   print('yes')
+# else:
+#   print('no')
+
+'''6.2'''
+
+# text =input('enter your text :')
+# word = input('enter your word :')
+# if text.find('word') == -1:
+#     print('yes')
+# else:
+#     print('no')
+
+'''Написать валидатор для почты. Пользователь вводит почту, а программа должна проверить,
+ что в почте есть символ '@' и '.', и если это так, то вывести "YES", иначе "NO'''
+
+'''6.3'''
+# text =input('enter your text :')
+# text_str = text.startswith('abc')
+# if text_str:
+#  replaced_text = text.replace('abc', 'www')
+#  print(replaced_text)
+# else:
+#  print(text.ljust(10, 'z'))
+
+'''6.4'''
+
+# str1 = input('enter your text :')
+# str2 = ''
+# for i in str1:
+#     if i not in('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
+#         print(str2+i)
+
+
+
+'''6.5'''
+
+post = input('enter your personal data :')
+
+if post.index('@' '.') == -1:
+    print('no')
+else:
+    print('yes')
+
+
+
+
+
+
+
+
+
