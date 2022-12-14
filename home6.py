15f8d14b76f8599542c9d2c9529e63ece96ac28d
Dmitry <dimalimankin@gmail.com>
2022-12-14 20:38:03 +0200

    second commit home6.py

--- home6.py
+++ home6.py
@@ -1,5 +1,4 @@
-
-'''6.1'''
+'''6.1+++'''
 # word = input('Enter your word:')
 # a = word[::-1]
 # if word == a:
@@ -7,49 +6,36 @@
 # else:
 #   print('no')
 
-'''6.2'''
-
-# text =input('enter your text :')
+'''6.2++++'''
+# text = input('enter your text :')
 # word = input('enter your word :')
-# if text.find('word') == -1:
+# if text.find(word) == -1:
+#     print('no')
+# else:
+#     print('yes')
+
+
+'''6.3+++'''
+text = input('enter your text :')
+text_str = text.startswith('abc')
+if text_str:
+    replaced_text = text.replace('abc', 'www')
+    print(replaced_text)
+else:
+    s1 = text
+    s2 = 'zzz'
+    s3 = '{}{}'.format(s1, s2)
+    print(s3)
+
+'''6.4+++'''
+# S = input('enter :')
+# s = ''.join(x for x in S if not x.isdigit())
+# print(s)
+
+'''6.5+++'''
+# post = input('enter your personal data :')
+# if '@' in post and '.' in post:
 #     print('yes')
 # else:
 #     print('no')
-
-
-'''6.3'''
-# text =input('enter your text :')
-# text_str = text.startswith('abc')
-# if text_str:
-#  replaced_text = text.replace('abc', 'www')
-#  print(replaced_text)
-# else:
-#  print(text.ljust(10, 'z'))
-
-'''6.4'''
-
-# str1 = input('enter your text :')
-# str2 = ''
-# for i in str1:
-#     if i not in('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
-#         print(str2+i)
-
-
-
-'''6.5'''
-
-post = input('enter your personal data :')
-
-if post.index('@' '.') == -1:
-    print('no')
-else:
-    print('yes')
-
-
-
-
-
-
-
-
 
