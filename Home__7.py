9140bf13cb66c53fb705243d7a5c4463edbe1094
Dmitry <dimalimankin@gmail.com>
2022-12-16 14:33:57 +0200

    first commit

---
+++ Home__7.py
@@ -0,0 +1,47 @@
+'''7.1'''
+# A = [int(input('a:')), int(input('b:')), int(input('c:')), int(input('d:')), int(input('e:'))]
+# print(A)
+
+'''7.2'''
+# A = [1, 2, 3, 4, 5]
+# A.pop()
+# print(A)
+
+'''7.3'''
+# A = [int(input('a:')), int(input('b:')), int(input('c :')), int(input('d :')), int(input('e:')),
+#      int(input('f:')), int(input('g:')), int(input('h:')), int(input('j:')), int(input('k:'))]
+# N = int(input('Enter N:'))
+# print(A.count(N))
+
+
+'''7.4'''
+# A = []
+# for i in range(int(input('Enter your favorite number N: '))):
+#     A.append(int(input('Enter number: ')))
+# A.reverse()
+# print(A)
+
+'''7.5'''
+
+# A = [int(input('a:')), int(input('b :')), int(input('c :')), int(input('d :')), int(input('e :'))]
+# C = []
+# # print(A)
+# for i in A:
+#     if i > 5:
+#         C.append(i)
+# print(C)
+
+'''7.6'''
+# A = []
+# for i in range(int(input('Enter your favorite number N: '))):
+#     A.append(int(input('Enter number: ')))
+# A.sort()
+# print(f'Максимальное число {A[-1]}\nМинимальное {A[1]}')
+
+'''7.7'''
+# N = 0
+# for i in input('Enter your text:').split():
+#     if i.isdigit():
+#         N += 1
+# print(N)
+
